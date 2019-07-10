eeb3a5b63d157851cf7532b4d2d83ab2998aa664
Jean-Bernard Pellerin <jeanbern@ualberta.ca>
2019-07-10 17:30:40 -0600

    Add a safe version to the benchmark

--- code/ZeroFormatterBenchmark.cs
+++ code/ZeroFormatterBenchmark.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using ZeroFormatter;
 
@@ -17,7 +18,7 @@ public static class StreamExtensions
 {
     private static readonly UTF8Encoding Utf8NoBom = new UTF8Encoding(false, true);
 
-    public static unsafe void Write<T>(this Stream stream, T value)
+    public static unsafe void UnsafeWrite<T>(this Stream stream, T value)
         where T : unmanaged
     {
         var pointer = Unsafe.AsPointer(ref value);
@@ -25,34 +26,31 @@ public static class StreamExtensions
         stream.Write(span);
     }
     
-    public static unsafe void Write<T>(this Stream stream, T[] values)
+    public static unsafe void UnsafeWrite<T>(this Stream stream, T[] values)
         where T : unmanaged
     {
-        var byteLength = sizeof(T) * values.Length;
-        stream.Write(byteLength);
+        stream.UnsafeWrite(values.Length);
         fixed (void* pointer = values)
         {
-            var span = new Span<byte>(pointer, byteLength);
+            var span = new Span<byte>(pointer, values.Length * sizeof(T));
             stream.Write(span);
         }
     }
 
-    public static unsafe void Write(this Stream stream, string value)
+    public static unsafe void UnsafeWrite(this Stream stream, string value)
     {
         var encoding = Utf8NoBom;
         var valueSpan = value.AsSpan();
         var len = encoding.GetByteCount(valueSpan);
 
-        var stack = stackalloc byte[len];
-        var byteSpan = new Span<byte>(stack, len);
+        Span<byte> span = stackalloc byte[len];
+        var encodedLen = encoding.GetBytes(valueSpan, span);
 
-        var encodedLen = encoding.GetBytes(valueSpan, byteSpan);
-
-        stream.Write(encodedLen);
-        stream.Write(byteSpan);
+        stream.UnsafeWrite(encodedLen);
+        stream.Write(span);
     }
     
-    public static unsafe ref T Read<T>(this Stream stream, ref T result)
+    public static unsafe ref T UnsafeRead<T>(this Stream stream, ref T result)
         where T : unmanaged
     {
         var thing = Unsafe.AsPointer(ref result);
@@ -61,7 +59,7 @@ public static class StreamExtensions
         return ref result;
     }
 
-    public static unsafe T Read<T>(this Stream stream)
+    public static unsafe T UnsafeRead<T>(this Stream stream)
         where T : unmanaged
     {
         var result = default(T);
@@ -73,18 +71,15 @@ public static class StreamExtensions
         return result;
     }
     
-    public static unsafe string ReadString(this Stream stream)
+    public static unsafe string UnsafeReadString(this Stream stream)
     {
-        var byteLength = stream.Read<int>();
-        var bytes = stackalloc byte[byteLength];
-        var byteSpan = new Span<byte>(bytes, byteLength);
-
-        stream.Read(byteSpan);
-
-        return Utf8NoBom.GetString(byteSpan);
+        var byteLength = stream.UnsafeRead<int>();
+        Span<byte> bytes = stackalloc byte[byteLength];
+        stream.Read(bytes);
+        return Utf8NoBom.GetString(bytes);
     }
 
-    public static unsafe T[] ReadArray<T>(this Stream stream)
+    public static unsafe T[] UnsafeReadArray<T>(this Stream stream)
         where T : unmanaged
     {
         if (typeof(T) == typeof(char))
@@ -92,22 +87,93 @@ public static class StreamExtensions
             Throw(new InvalidCastException("ReadArray<char>() should be replaced with a call to ReadCharArray()."));
         }
 
-        var byteLength = stream.Read<int>();
-        if (byteLength % sizeof(T) != 0)
-        {
-            Throw(new InvalidDataException($"sizeof({typeof(T).Name}) = {sizeof(T).ToString()} does not evenly divide the stream length: {byteLength.ToString()}."));
-        }
-
-        var length = byteLength / sizeof(T);
+        var length = stream.UnsafeRead<int>();
 #pragma warning disable U2U1023 // Do not overwrite initialized variables
         var results = new T[length];
 #pragma warning restore U2U1023 // Do not overwrite initialized variables
 
         fixed (void* pointer = results)
         {
-            var span = new Span<byte>(pointer, byteLength);
+            var span = new Span<byte>(pointer, length);
             stream.Read(span);
         }
+
+        return results;
+    }
+
+    public static void Write<T>(this Stream stream, T value)
+        where T : unmanaged
+    {
+        var tSpan = MemoryMarshal.CreateSpan(ref value, 1);
+        var span = MemoryMarshal.AsBytes(tSpan);
+        stream.Write(span);
+    }
+    
+    public static void Write<T>(this Stream stream, T[] values)
+        where T : unmanaged
+    {
+        var tSpan = MemoryMarshal.CreateSpan(ref values[0], values.Length);
+        var span = MemoryMarshal.AsBytes(tSpan);
+        stream.Write(values.Length);
+        stream.Write(span);
+    }
+
+    public static void Write(this Stream stream, string value)
+    {            
+        var encoding = Utf8NoBom;
+        var valueSpan = value.AsSpan();
+        var len = encoding.GetByteCount(valueSpan);
+
+        Span<byte> byteSpan = stackalloc byte[len];
+        var encodedLen = encoding.GetBytes(valueSpan, byteSpan);
+        
+        stream.Write(encodedLen);
+        stream.Write(byteSpan);
+    }
+    
+    public static ref T Read<T>(this Stream stream, ref T result)
+        where T : unmanaged
+    {
+        var tSpan = MemoryMarshal.CreateSpan(ref result, 1);
+        var span = MemoryMarshal.AsBytes(tSpan);
+        stream.Read(span);
+        return ref result;
+    }
+
+    public static T Read<T>(this Stream stream)
+        where T : unmanaged
+    {
+        var result = default(T);
+        var tSpan = MemoryMarshal.CreateSpan(ref result, 1);
+        var span = MemoryMarshal.AsBytes(tSpan);
+        stream.Read(span);
+        return result;
+    }
+    
+    public static string ReadString(this Stream stream)
+    {
+        var byteLength = stream.Read<int>();
+        Span<byte> bytes = stackalloc byte[byteLength];
+        stream.Read(bytes);
+        return Utf8NoBom.GetString(bytes);
+    }
+
+    public static T[] ReadArray<T>(this Stream stream)
+        where T : unmanaged
+    {
+        if (typeof(T) == typeof(char))
+        {
+            Throw(new InvalidCastException("ReadArray<char>() should be replaced with a call to ReadCharArray()."));
+        }
+
+        var length = stream.Read<int>();
+#pragma warning disable U2U1023 // Do not overwrite initialized variables
+        var results = new T[length];
+#pragma warning restore U2U1023 // Do not overwrite initialized variables
+        
+        var tSpan = MemoryMarshal.CreateSpan(ref results[0], results.Length);
+        var span = MemoryMarshal.AsBytes(tSpan);
+        stream.Read(span);
 
         return results;
     }
@@ -153,6 +219,24 @@ public class Person : IEquatable<Person>
     public bool Equals(Person other)
     {
         return Age == other.Age && FirstName == other.FirstName && LastName == other.LastName && Sex == other.Sex;
+    }
+
+    public void UnsafeSerialize(Stream stream)
+    {
+        stream.UnsafeWrite(Age);
+        stream.UnsafeWrite(FirstName);
+        stream.UnsafeWrite(LastName);
+        stream.UnsafeWrite(Sex);
+    }
+
+    public static Person UnsafeDeserialize(Stream stream)
+    {
+        var person = new Person();
+        stream.UnsafeRead(ref person._age);
+        person.FirstName = stream.UnsafeReadString();
+        person.LastName = stream.UnsafeReadString();
+        stream.UnsafeRead(ref person._sex);
+        return person;
     }
 
     public void Serialize(Stream stream)
@@ -222,7 +306,7 @@ public struct Vector3 : IEquatable<Vector3>
 
 internal static class SerializationTesting
 {
-    private const int WarmUpIteration = 2;
+    private const int WarmUpIteration = 100;
     private const int RunIteration = 100000;
 
     private static int _iteration = 1;
@@ -241,49 +325,66 @@ internal static class SerializationTesting
         _iteration = WarmUpIteration;
         SerializeZeroFormatter(p);
         Serialize(p);
+        UnsafeSerialize(p);
         SerializeZeroFormatter(l);
         Serialize(l);
+        UnsafeSerialize(l);
         SerializeZeroFormatter(integer);
         SerializeCustomUnmanaged(integer);
+        UnsafeSerializeCustomUnmanaged(integer);
         SerializeZeroFormatter(v3);
         SerializeCustomUnmanaged(v3);
+        UnsafeSerializeCustomUnmanaged(v3);
         SerializeZeroFormatter(largeString);
         Serialize(largeString);
+        UnsafeSerialize(largeString);
         SerializeZeroFormatter(v3List);
         SerializeCustomList(v3List);
+        UnsafeSerializeCustomList(v3List);
 
         _dryRun = false;
         _iteration = RunIteration;
         Console.WriteLine("Person");
         var a1 = SerializeZeroFormatter(p);
         var b1 = Serialize(p);
+        var c1 = UnsafeSerialize(p);
         Console.WriteLine("Person[]");
         var a2 = SerializeZeroFormatter(l);
         var b2 = Serialize(l);
+        var c2 = UnsafeSerialize(l);
         Validate("ZeroFormatter", p, l, a1, a2);
         Validate("CoolFormatter", p, l, b1, b2);
+        Validate("UnsafeFormatter", p, l, c1, c2);
         
         Console.WriteLine("int");
         var a3 = SerializeZeroFormatter(integer);
         var b3 = SerializeCustomUnmanaged(integer);
+        var c3 = UnsafeSerializeCustomUnmanaged(integer);
         Console.WriteLine("Vector3");
         var a4 = SerializeZeroFormatter(v3);
         var b4 = SerializeCustomUnmanaged(v3);
+        var c4 = UnsafeSerializeCustomUnmanaged(v3);
         Console.WriteLine("string");
         var a5 = SerializeZeroFormatter(largeString);
         var b5 = Serialize(largeString);
+        var c5 = UnsafeSerialize(largeString);
         Console.WriteLine("Vector3[]");
         var a6 = SerializeZeroFormatter(v3List);
         var b6 = SerializeCustomList(v3List);
+        var c6 = UnsafeSerializeCustomList(v3List);
 
         Validate2("ZeroFormatter", integer, a3);
         Validate2("CoolFormatter", integer, b3);
+        Validate2("UnsafeFormatter", integer, c3);
         Validate2("ZeroFormatter", v3, a4);
         Validate2("CoolFormatter", v3, b4);
+        Validate2("UnsafeFormatter", v3, c4);
         Validate2("ZeroFormatter", largeString, a5);
         Validate2("CoolFormatter", largeString, b5);
+        Validate2("UnsafeFormatter", largeString, c5);
         Validate2("ZeroFormatter", v3List, a6);
         Validate2("CoolFormatter", v3List, b6);
+        Validate2("UnsafeFormatter", v3List, c6);
     }
 
     private static void Validate(string label, Person original, IList<Person> originalList, Person copy, IList<Person> copyList)
@@ -527,6 +628,200 @@ internal static class SerializationTesting
                 {
                     stream.Position = 0;
                     stream.Read(ref copy);
+                }
+            }
+        }
+
+        if (!_dryRun)
+        {
+            Console.WriteLine($"{"Binary Size",15}   {ToHumanReadableSize(length)}");
+        }
+
+        return copy;
+    }
+
+    
+    private static string UnsafeSerialize(string original)
+    {
+        Console.WriteLine("UnsafeFormatter");
+
+        string copy = default;
+        long length;
+
+        using (var stream = new MemoryStream())
+        {
+            using (new Measure("Serialize"))
+            {
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    stream.UnsafeWrite(original);
+                }
+            }
+
+            length = stream.Position;
+            using (new Measure("Deserialize"))
+            {
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    copy = stream.UnsafeReadString();
+                }
+            }
+        }
+
+        if (!_dryRun)
+        {
+            Console.WriteLine($"{"Binary Size",15}   {ToHumanReadableSize(length)}");
+        }
+
+        return copy;
+    }
+
+    private static Person UnsafeSerialize(Person original)
+    {
+        Console.WriteLine("UnsafeFormatter");
+        Person copy = default;
+        long length;
+
+        using (var stream = new MemoryStream())
+        {
+            using (new Measure("Serialize"))
+            {
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    original.UnsafeSerialize(stream);
+                }
+            }
+
+            length = stream.Position;
+            using (new Measure("Deserialize"))
+            {
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    copy = Person.UnsafeDeserialize(stream);
+                }
+            }
+        }
+
+        if (!_dryRun)
+        {
+            Console.WriteLine($"{"Binary Size",15}   {ToHumanReadableSize(length)}");
+        }
+
+        return copy;
+    }
+
+    private static IList<Person> UnsafeSerialize(IList<Person> original)
+    {
+        Console.WriteLine("UnsafeFormatter");
+        long length;
+
+        Person[] copy = default;
+        using (var stream = new MemoryStream())
+        {
+            using (new Measure("Serialize"))
+            {
+                var count = original.Count;
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    stream.Write(count);
+                    for (var j = 0; j < count; j++)
+                    {
+                        original[j].UnsafeSerialize(stream);
+                    }
+                }
+            }
+
+            length = stream.Position;
+            using (new Measure("Deserialize"))
+            {
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    var count = stream.Read<int>();
+                    copy = new Person[count];
+                    for (var j = 0; j < count; j++)
+                    {
+                        copy[j]  = Person.UnsafeDeserialize(stream);
+                    }
+                }
+            }
+        }
+
+        if (!_dryRun)
+        {
+            Console.WriteLine($"{"Binary Size",15}   {ToHumanReadableSize(length)}");
+        }
+
+        return copy;
+    }
+    private static T[] UnsafeSerializeCustomList<T>(T[] original)
+        where T : unmanaged
+    {
+        Console.WriteLine("UnsafeFormatter");
+        long length;
+
+        T[] copy = default;
+        using (var stream = new MemoryStream())
+        {
+            using (new Measure("Serialize"))
+            {
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    stream.UnsafeWrite(original);
+                }
+            }
+
+            length = stream.Position;
+            using (new Measure("Deserialize"))
+            {
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    copy = stream.UnsafeReadArray<T>();
+                }
+            }
+        }
+
+        if (!_dryRun)
+        {
+            Console.WriteLine($"{"Binary Size",15}   {ToHumanReadableSize(length)}");
+        }
+
+        return copy;
+    }
+
+    private static T UnsafeSerializeCustomUnmanaged<T>(T original)
+        where T : unmanaged
+    {
+        Console.WriteLine("UnsafeFormatter");
+
+        var copy = default(T);
+        long length;
+
+        using (var stream = new MemoryStream())
+        {
+            using (new Measure("Serialize"))
+            {
+                for (var i = 1; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    stream.UnsafeWrite(original);
+                }
+            }
+
+            length = stream.Position;
+            using (new Measure("Deserialize"))
+            {
+                for (var i = 0; i < _iteration; i++)
+                {
+                    stream.Position = 0;
+                    stream.UnsafeRead(ref copy);
                 }
             }
         }
